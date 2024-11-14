1f07e40d6951295397fdd3c2e6ee9d4083711302
ameen <ameenmustafa2107@gmail.com>
2024-11-14 10:50:28 +0530

    mongodb congif set and restart added

--- mongodb.sh
+++ mongodb.sh
@@ -1,6 +1,7 @@
 cp mongo.repo /etc/yum.repos.d/mongo.repo
 dnf install mongodb-org -y
-# we should change 127.0.0.0 to 0.0.0.0 in the file
-sed -e 's|127.0.0.1|0.0.0.0|' /etc/mongod.conf
 systemctl enable mongod
 systemctl start mongod
+# we should change 127.0.0.0 to 0.0.0.0 in the file
+sed -e 's|127.0.0.1|0.0.0.0|' /etc/mongod.conf
+systemctl restart mongod
