579f45c4bd58455178680dde810b6692b4ca7ade
ameen <ameenmustafa2107@gmail.com>
2024-11-14 20:28:08 +0530

    update ip

--- mysql.sh
+++ mysql.sh
@@ -1,7 +1,20 @@
+echo -e "\e[36m>>>>>>>>>>>>>>> module disable <<<<<<<<<<<<<\e[0m"
 dnf module disable mysql -y
-cp mysql.repo /etc/yum.repos.d/mysql.repo
+
+echo -e "\e[36m>>>>>>>>>>>>>>> mysql repo copy <<<<<<<<<<<<<\e[0m"
+cp /home/centos/roboshop-shell/mysql.repo /etc/yum.repos.d/mysql.repo
+
+echo -e "\e[36m>>>>>>>>>>>>>>> install mysql <<<<<<<<<<<<<\e[0m"
 dnf install mysql-community-server -y
+
+echo -e "\e[36m>>>>>>>>>>>>>>> enable mysql <<<<<<<<<<<<<\e[0m"
 systemctl enable mysqld
+
+echo -e "\e[36m>>>>>>>>>>>>>>> start mysql <<<<<<<<<<<<<\e[0m"
 systemctl start mysqld
+
+echo -e "\e[36m>>>>>>>>>>>>>>> setting password <<<<<<<<<<<<<\e[0m"
 mysql_secure_installation --set-root-pass RoboShop@1
+
+echo -e "\e[36m>>>>>>>>>>>>>>> logging <<<<<<<<<<<<<\e[0m"
 mysql -uroot -pRoboShop@1
